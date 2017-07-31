db18e214b9df4876451d1a9fbf1106600d595abe
Mike Gering <mike@mfg-consulting-llc.com>
2017-07-31 19:43:46 -0400

    wip

--- main/forms.py
+++ main/forms.py
@@ -1,11 +1,16 @@
 from django import forms
-
+from .models import Category
 
 class PreferencesForm(forms.Form):
+
     def __init__(self, *args, **kwargs):
         category_prefs = kwargs.pop('category_prefs')
         super(PreferencesForm, self).__init__(*args, **kwargs)
-        for category_pk in category_prefs:
-            checkbox = forms.CheckboxInput()
-            checkbox.initial = True
-            self.fields['category_%s' % category_pk] = checkbox
+        for category in Category.objects.order_by('name'):
+            checkbox = forms.BooleanField(label=category.name, required=False)
+            checkbox.category_pk = category.pk
+            if category.pk in category_prefs:
+                checkbox.initial = category_prefs[category.pk]
+            else:
+                checkbox.initial = True
+            self.fields['category_%s' % category.pk] = checkbox
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
+import json
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.template import loader
 from .models import Category, Quote
 from .forms import PreferencesForm
@@ -22,16 +24,37 @@ def about(request):
     return HttpResponse(template.render(context, request))
 
 def preferences(request):
+    category_prefs = get_category_prefs(request)
     if request.method == 'POST':
-        #TODO: FIX
-        pass
+        form = PreferencesForm(request.POST, category_prefs=category_prefs)
+        if form.is_valid():
+            response =  redirect('/')
+            set_category_prefs(response, form)
+        else:
+            form = PreferencesForm(category_prefs=category_prefs)
+            response = render(request, 'main/preferences.html', {'form': form})
     else:
-        category_prefs = {}
-        for category in Category.objects.order_by('name'):
-            category_prefs[category.pk] = False
+        category_prefs = get_category_prefs(request)
         form = PreferencesForm(category_prefs=category_prefs)
-    return render(request, 'main/preferences.html', {'form': form})
+        response = render(request, 'main/preferences.html', {'form': form})
+    return response
 
+def get_category_prefs(request):
+    if 'category_prefs' in request.COOKIES:
+        prefs_json = request.COOKIES['category_prefs']
+        category_prefs = json.loads(prefs_json)
+    else:
+        category_prefs = dict()
+        for category in Category.objects.order_by('name'):
+            category_prefs[category.pk] = True
+    return category_prefs
 
+def set_category_prefs(response, form):
+    category_prefs = dict()
+    for field_name in form.fields:
+        field = form.fields[field_name]
+        if hasattr(field, 'category_pk'):
+            category_prefs[field.category_pk] = form.cleaned_data[field_name]
+    return category_prefs
 
 
