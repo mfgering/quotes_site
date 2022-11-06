ed34f616945f4501523d01f3c5970e24c741e347
Mike Gering <git@mfg.ferree-gering.com>
2022-11-06 07:14:19 -0500

    refactor

--- urls.py
+++ urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
 from django.contrib import admin
 from django.conf import settings
-
-import quotes
-
+import quotes.views
 
 urlpatterns = [
+    path('', quotes.views.HomeView.as_view(), name='index'),
     path('quotes/', include('quotes.urls')),
+
     path('admin/', admin.site.urls),
 ]
 
